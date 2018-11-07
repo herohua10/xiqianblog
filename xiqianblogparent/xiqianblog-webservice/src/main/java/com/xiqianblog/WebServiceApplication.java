9ccb61320ad4e3991cf95715aa39a9b4bfcdf52d
huajian <hero514346219@163.com>
2018-11-07 20:49:28 +0800

    测试hystrix断路器模式及后备模式

--- xiqianblogparent/xiqianblog-webservice/src/main/java/com/xiqianblog/WebServiceApplication.java
+++ xiqianblogparent/xiqianblog-webservice/src/main/java/com/xiqianblog/WebServiceApplication.java
@@ -2,12 +2,14 @@ package com.xiqianblog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 import tk.mybatis.spring.annotation.MapperScan;
 
+@EnableCircuitBreaker
 @EnableDiscoveryClient
 @SpringBootApplication
 @MapperScan("com.xiqianblog.dao")
--- xiqianblogparent/xiqianblog-webservice/src/main/java/com/xiqianblog/service/impl/UserServiceImpl.java
+++ xiqianblogparent/xiqianblog-webservice/src/main/java/com/xiqianblog/service/impl/UserServiceImpl.java
@@ -1,5 +1,7 @@
 package com.xiqianblog.service.impl;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import com.xiqianblog.dao.UserDao;
 import com.xiqianblog.datamodel.User;
 import com.xiqianblog.message.common.User.QueryUsersReq;
@@ -7,7 +9,9 @@ import com.xiqianblog.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 /**
  * 【描述内容】
@@ -23,9 +27,52 @@ public class UserServiceImpl implements UserService
 	@Autowired
 	private UserDao userDao;
 
+	// 断路器超时时间7s
+	@HystrixCommand(commandProperties = {@HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "7000")},
+                    fallbackMethod = "buildFallbackUsers")
 	@Override
 	public List<User> queryUser()
 	{
+        randomlyRunLong();
+
 		return userDao.selectAll();
 	}
+
+	// 后备方法
+	private List<User> buildFallbackUsers()
+    {
+        List<User> users = new ArrayList<>();
+
+        User user = new User();
+        user.setId(32619834);
+        user.setName("jy");
+        user.setPassword("21433558");
+
+        users.add(user);
+        return users;
+    }
+
+	private void randomlyRunLong()
+    {
+        Random random = new Random();
+
+        int randonNum = random.nextInt((3 - 1) + 1) + 1;
+
+        if (randonNum == 3)
+        {
+            sleep();
+        }
+    }
+
+	private void sleep()
+    {
+        try
+        {
+            Thread.sleep(11000);
+        }
+        catch (InterruptedException e)
+        {
+            e.printStackTrace();
+        }
+    }
 }
