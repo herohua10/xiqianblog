c0ce8dd006d18523a30b4717882027c6092ffc4e
herohua10 <hero514346219@163.com>
2018-11-10 15:29:17 +0800

    创建Euraka服务注册中心

---
+++ xiqianblogparent/xiqianblog-registry/src/main/java/com/xiqianblog/RegistryApplication.java
@@ -0,0 +1,13 @@
+package com.xiqianblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer     //开启Eureka注册服务
+public class RegistryApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RegistryApplication.class, args);
+    }
+}
