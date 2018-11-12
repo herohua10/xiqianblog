5949c4219213a2ad7d5e8c5879ee3116f3147de2
herohua10 <hero514346219@163.com>
2018-11-12 22:53:39 +0800

    初始化配置中心

--- xiqianblogparent/xiqianblog-authentication/src/main/java/com/xiqianblog/AuthenticationApplication.java
+++ xiqianblogparent/xiqianblog-authentication/src/main/java/com/xiqianblog/AuthenticationApplication.java
@@ -3,6 +3,11 @@ package com.xiqianblog;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 
+/**
+ * @author: Jian Hua
+ * @date: 2018/11/12 21:25
+ **/
+
 @SpringBootApplication
 @EnableAuthorizationServer
 public class AuthenticationApplication {
---
+++ xiqianblogparent/xiqianblog-config/src/main/java/com/xiqianblog/ConfigServerApplication.java
@@ -0,0 +1,20 @@
+package com.xiqianblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * @author: Jian Hua
+ * @date: 2018/11/12 22:19
+ **/
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableConfigServer
+public class ConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class, args);
+    }
+}
