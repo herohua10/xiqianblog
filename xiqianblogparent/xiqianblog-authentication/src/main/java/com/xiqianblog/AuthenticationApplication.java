a891854691a19f3fb75c184674c1654fc4a1f64e
huajian <hero514346219@163.com>
2018-11-12 21:31:03 +0800

    初始化验证服务

---
+++ xiqianblogparent/xiqianblog-authentication/src/main/java/com/xiqianblog/AuthenticationApplication.java
@@ -0,0 +1,11 @@
+package com.xiqianblog;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+
+@SpringBootApplication
+@EnableAuthorizationServer
+public class AuthenticationApplication {
+
+
+}
